13525e0cf5a7e4e02418d5cc661d506b6d9cacf4
Victor Li <victorli5674@yahoo.com>
2018-07-24 02:09:38 -0700

    able to add messages to channel

---
+++ resolvers/message.js
@@ -0,0 +1,27 @@
+import requiresAuth from '../permissions';
+
+export default {
+  Query: {
+    messages: requiresAuth.createResolver(async (parent, { channelId }, { models }) => models.Message.findAll(
+      { order: [['created_at', 'ASC']], where: { channelId } },
+      { raw: true }
+    ))
+  },
+  Mutation: {
+    createMessage: requiresAuth.createResolver(async (parent, args, { models, user }) => {
+      try {
+        await models.Message.create({
+          ...args,
+          userId: user.id
+        });
+        return true;
+      } catch (err) {
+        console.log(err);
+        return false;
+      }
+    })
+  },
+  Message: {
+    user: ({ userId }, args, { models }) => models.User.findOne({ where: { id: userId } }, { raw: true })
+  }
+};
